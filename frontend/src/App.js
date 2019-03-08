9ea3b023d3e02caf981b43af5164c543c58edb48
Luca Fraser <luca@deinternetjongens.nl>
2019-03-08 14:22:56 +0100

    implemented the loadingscreen and fetch data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
 import Header from "./components/organisms/Header";
-import {askForPermissioToReceiveNotifications} from "./push-notification";
-
-const NotificationButton = () => (
-  <button onClick={askForPermissioToReceiveNotifications} >
-    Click
-  </button>
-);
+import LoadingScreen from "./components/pages/LoadingScreen";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  };
+
+  componentDidMount() {
+    fetch("https://office-assistant.defser.nl/backend/public/getpresent")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-      <>
-        <Header />
-        <NotificationButton/>
-      </>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <LoadingScreen/>;
+    } else {
+      return (
+        <>
+          <Header />
+          <ul>
+            {items.map(item => (
+              <li key={item.device_owner}>
+                {item.device_owner}
+              </li>
+            ))}
+          </ul>
+        </>
+      );
+    }
   }
 }
 
---
+++ frontend/src/components/pages/LoadingScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import {color, theme} from "../templates/ui";
+import Logo from "../atoms/icons/Logo";
+import Loader from "../atoms/icons/Loader";
+
+const StyledPage = styled.div`
+  background-color: ${color.body.base};
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const StyledLogo = styled(Logo)`
+  display: block;
+  margin: 0 auto;
+`;
+
+const StyledLoader = styled(Loader)`
+  display: block;
+  margin: 0 auto;
+  margin-top: 50px;
+`;
+
+const StyledMessage = styled.p`
+  color: ${color.base.base};
+  text-align: center;
+  padding-top: 15px;
+  font-size: 1.8em;
+  font-family: "Lato", sans-serif;
+`;
+
+const LoadingScreen = ({ children }) => (
+  <StyledPage>
+    <Container>
+      <StyledLogo />
+      <StyledMessage>{children}</StyledMessage>
+      <StyledLoader />
+    </Container>
+  </StyledPage>
+);
+
+export default LoadingScreen;
