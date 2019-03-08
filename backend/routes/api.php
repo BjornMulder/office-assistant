523870398dcefd21c0743a796b9d891d8aee1011
bjorn mulder <bjornmulder.dev@gmail.com>
2019-03-08 14:17:53 +0100

    Move routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/getpresent', 'Api\AppController@getPresent');
+Route::post('/arriving', 'Api\PiController@arriving');
+Route::post('/leaving', 'Api\PiController@leaving');
